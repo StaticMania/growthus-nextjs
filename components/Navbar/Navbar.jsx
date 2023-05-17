db85c5594776fe3ce8774fb069b38c971c084dd4
ronoconstructo <rono.devs@constructo.online>
2023-05-17 17:53:15 +0600

    added navbar

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,19 +1,163 @@
+"use client";
 import Link from "next/link";
 import React from "react";
-
+import {useEffect, useState} from "react";
+import menuData from "./menuData";
+import Image from "next/image";
 const Navbar = () => {
+  // Navbar toggle
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const navbarToggleHandler = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  // Sticky Navbar
+  const [sticky, setSticky] = useState(false);
+  const handleStickyNavbar = () => {
+    if (window.scrollY >= 50) {
+      setSticky(true);
+    } else {
+      setSticky(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleStickyNavbar);
+  }, []);
+
+  // submenu handler
+  const [collapsed, setCollapsed] = useState(true);
+
   return (
-    <div className="">
-      <div className="">
-        <Link href="/">Grouwthus</Link>
-        <div className="">
-          <Link href="/about">About</Link>
-          <Link href="/services">Services</Link>
-          <Link href="/job">Job</Link>
-          <Link href="/blog">Blog</Link>
+    <>
+      <nav
+        className={`
+          ${
+            sticky
+              ? "navbar navbar-expand-lg position-fixed w-100 zindex-dropdown"
+              : "navbar navbar-expand-lg position-fixed w-100 zindex-dropdown sticky-nav"
+          }`}
+        id="navigationBar"
+      >
+        <div class="container">
+          <Link
+            className="navbar-brand"
+            href="/"
+          >
+            <Image
+              src="/images/logo.svg"
+              alt="logo"
+              width={140}
+              height={30}
+            />
+          </Link>
+          <button
+            type="button"
+            data-bs-toggle="collapse"
+            className={collapsed ? "navbar-toggler collapsed p-3" : "navbar-toggler p-3"}
+            onClick={() => {
+              setCollapsed(!collapsed);
+            }}
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded={collapsed ? false : true}
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-default">
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="white"
+                  d="M17.4,6.2H0.6C0.3,6.2,0,5.9,0,5.5V4.1c0-0.4,0.3-0.7,0.6-0.7h16.9c0.3,0,0.6,0.3,0.6,0.7v1.4C18,5.9,17.7,6.2,17.4,6.2z M17.4,14.1H0.6c-0.3,0-0.6-0.3-0.6-0.7V12c0-0.4,0.3-0.7,0.6-0.7h16.9c0.3,0,0.6,0.3,0.6,0.7v1.4C18,13.7,17.7,14.1,17.4,14.1z"
+                ></path>
+              </svg>
+            </span>
+            <span class="navbar-toggler-toggled">
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 18 18"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill="white"
+                  d="M11.5,9.5l5-5c0.2-0.2,0.2-0.6-0.1-0.9l-1-1c-0.3-0.3-0.7-0.3-0.9-0.1l-5,5l-5-5C4.3,2.3,3.9,2.4,3.6,2.6l-1,1 C2.4,3.9,2.3,4.3,2.5,4.5l5,5l-5,5c-0.2,0.2-0.2,0.6,0.1,0.9l1,1c0.3,0.3,0.7,0.3,0.9,0.1l5-5l5,5c0.2,0.2,0.6,0.2,0.9-0.1l1-1 c0.3-0.3,0.3-0.7,0.1-0.9L11.5,9.5z"
+                ></path>
+              </svg>
+            </span>
+          </button>
+          <div
+            className={collapsed ? "collapse navbar-collapse" : "collapse navbar-collapse show"}
+            id="navbarSupportedContent"
+          >
+            {menuData.map((menuItem, index) => (
+              <ul
+                class="navbar-nav ms-xl-5"
+                key={menuItem.id}
+              >
+                {menuItem.path ? (
+                  <li className={`nav-item`}>
+                    <Link
+                      href={menuItem.path}
+                      className={`nav-link`}
+                    >
+                      {menuItem.title}
+                    </Link>
+                  </li>
+                ) : (
+                  <>
+                    <li className={`nav-item dropdown`}>
+                      <a
+                        onClick={() => handleSubmenu(index)}
+                        className="nav-link dropdown-toggle"
+                        role="button"
+                        data-bs-toggle="dropdown"
+                      >
+                        {menuItem.title}
+                      </a>
+                      <ul class="dropdown-menu">
+                        <li class="dropdown-menu-wrapper">
+                          <ul class="list-unstyled">
+                            {menuItem.submenu.map((submenuItem) => (
+                              <li
+                                className="dropdown-menu-item"
+                                key={submenuItem.id}
+                              >
+                                <Link
+                                  href={submenuItem.path}
+                                  className="block rounded py-2.5 text-sm text-dark hover:opacity-70 dark:text-white lg:px-3"
+                                >
+                                  {submenuItem.title}
+                                </Link>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </>
+                )}
+              </ul>
+            ))}
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item ms-lg-6 ms-sm-0">
+                <a
+                  className="btn btn-navigation w-100"
+                  href="/"
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  Let&apos;s Talk
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </div>
+      </nav>
+    </>
   );
 };
 
