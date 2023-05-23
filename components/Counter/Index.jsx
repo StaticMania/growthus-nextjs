65cdaf953996e02450b8ce049f275d9e6730d76d
Arifulislam5577 <arifalamm33@gmal.com>
2023-05-23 21:34:52 +0600

    counter issue fixed

--- components/Counter/Index.jsx
+++ components/Counter/Index.jsx
@@ -1,51 +1,48 @@
 "use client";
-"Use Client";
-import React from "react";
-import CountUp from "react-countup";
+
 import CounterData from "./CounterData";
-import {useState} from "react";
-import ScrollTrigger from "react-scroll-trigger";
+import { useState, useEffect } from "react";
 const Counter = () => {
-  const [counterOn, setCounterOn] = useState(true);
+  const CountUpNumber = ({ number }) => {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+      if (count < number) {
+        const timer = setTimeout(() => {
+          setCount(count + 2);
+        }, 10);
+
+        return () => clearTimeout(timer);
+      }
+    }, [count, number]);
+
+    return <span className="counter">{count}</span>;
+  };
+
   return (
-    <ScrollTrigger
-      onEnter={() => setCounterOn(true)}
-      onExit={() => setCounterOn(false)}
-    >
-      <section className="counter-up pb-lg-7">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 mx-auto">
-              <div className="counter-wraper py-4 py-md-6 d-md-flex">
-                {CounterData.map((item, i) => {
-                  return (
-                    <div
-                      className="counter-up-content mb-3"
-                      key={i}
-                    >
-                      <div className="counter-up-content-item">
-                        <div className="counter-percent">
-                          {counterOn && (
-                            <CountUp
-                              start={0}
-                              end={`${item.number}`}
-                              delay={0}
-                              className="counter"
-                            />
-                          )}
-                          <span className="percent">+</span>
-                        </div>
-                        <h3>{item.text}</h3>
+    <section className="counter-up pb-lg-7">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 mx-auto">
+            <div className="counter-wraper py-4 py-md-6 d-md-flex">
+              {CounterData.map((item, i) => {
+                return (
+                  <div className="counter-up-content mb-3" key={i}>
+                    <div className="counter-up-content-item">
+                      <div className="counter-percent">
+                        {<CountUpNumber number={item.number} />}
+                        <span className="percent">+</span>
                       </div>
+                      <h3>{item.text}</h3>
                     </div>
-                  );
-                })}
-              </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
-      </section>
-    </ScrollTrigger>
+      </div>
+    </section>
   );
 };
 export default Counter;
