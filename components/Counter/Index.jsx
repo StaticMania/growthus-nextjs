1b2f8043f919fea4883d908532770abc4d07dfbf
ronoconstructo <rono.devs@constructo.online>
2023-05-24 14:43:20 +0600

    fixed coount up menu

--- components/Counter/Index.jsx
+++ components/Counter/Index.jsx
@@ -1,44 +1,37 @@
 "use client";
+import React from "react";
+import CountUp from "react-countup";
 import {counterData} from "@/Data/Data.js";
-import {useState, useEffect} from "react";
 const Counter = () => {
-  const CountUpNumber = ({number}) => {
-    const [count, setCount] = useState(0);
-
-    useEffect(() => {
-      if (count < number) {
-        const timer = setTimeout(() => {
-          setCount(count + 2);
-        }, 10);
-
-        return () => clearTimeout(timer);
-      }
-    }, [count, number]);
-
-    return <span className="counter">{count}</span>;
-  };
   return (
     <section className="counter-up pb-lg-7">
       <div className="container">
         <div className="row">
           <div className="col-lg-8 mx-auto">
             <div className="counter-wraper py-4 py-md-6 d-md-flex">
-              {counterData.map((item, i) => {
-                return (
-                  <div
-                    className="counter-up-content mb-3"
-                    key={i}
-                  >
-                    <div className="counter-up-content-item">
-                      <div className="counter-percent">
-                        {<CountUpNumber number={item.number} />}
-                        <span className="percent">+</span>
-                      </div>
-                      <h3>{item.text}</h3>
+              {counterData.map((item, i) => (
+                <div
+                  className="counter-up-content mb-3"
+                  key={i}
+                >
+                  <div className="counter-up-content-item">
+                    <div className="counter-percent">
+                      {
+                        <CountUp
+                          start={0}
+                          end={`${item.number}`}
+                          enableScrollSpy
+                          scrollSpyOnce
+                          delay={0}
+                          className="counter"
+                        />
+                      }
+                      <span className="percent">+</span>
                     </div>
+                    <h3>{item.text}</h3>
                   </div>
-                );
-              })}
+                </div>
+              ))}
             </div>
           </div>
         </div>
